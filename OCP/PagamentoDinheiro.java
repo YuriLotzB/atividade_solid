bde4b2d3f193a95d4d6886d5d7c23aaf180a5ae6
YuriLotzB <yuribrun@edu.unifil.br>
2026-05-22 23:57:26 -0300

    Update PagamentoDinheiro.java

--- OCP/PagamentoDinheiro.java
+++ OCP/PagamentoDinheiro.java
@@ -3,6 +3,6 @@ package OCP;
 public class PagamentoDinheiro implements FormaPagamento {
     @Override
     public void pagar(double valor) {
-        System.out.printf("[Pagamento] Pagamento de R$ %.2f realizado em Dinheiro!%n", valor);
+        System.out.printf("Pagamento de R$ %.2f realizado em Dinheiro!%n", valor);
     }
 }
