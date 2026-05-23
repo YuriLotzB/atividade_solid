3c2058dbe2569d56df0b60b69d5e9570c1ee22c7
YuriLotzB <yuribrun@edu.unifil.br>
2026-05-22 23:57:38 -0300

    Update PagamentoCartao.java

--- OCP/PagamentoCartao.java
+++ OCP/PagamentoCartao.java
@@ -3,6 +3,6 @@ package OCP;
 public class PagamentoCartao implements FormaPagamento {
     @Override
     public void pagar(double valor) {
-        System.out.printf("[Pagamento] Pagamento de R$ %.2f realizado via Cartão de Crédito!%n", valor);
+        System.out.printf("Pagamento de R$ %.2f realizado via Cartão de Crédito!%n", valor);
     }
 }
