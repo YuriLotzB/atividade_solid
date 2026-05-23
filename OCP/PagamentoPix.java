c82cc701efd54aa7b5c969167c0a26f7c0212465
YuriLotzB <yuribrun@edu.unifil.br>
2026-05-22 23:57:16 -0300

    Update PagamentoPix.java

--- OCP/PagamentoPix.java
+++ OCP/PagamentoPix.java
@@ -3,6 +3,6 @@ package OCP;
 public class PagamentoPix implements FormaPagamento {
     @Override
     public void pagar(double valor) {
-        System.out.printf("[Pagamento] Pagamento de R$ %.2f realizado via PIX!%n", valor);
+        System.out.printf("Pagamento de R$ %.2f realizado via PIX!%n", valor);
     }
 }
